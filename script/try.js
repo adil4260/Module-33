65a3a3376eef3cc2a76c56c2e05ec95d5dba82b9
adil4260 <adilsarkar001@gmail.com>
2026-09-15 01:07:45 +0600

    second commit

---
+++ script/try.js
@@ -0,0 +1,10 @@
+const loadLesson = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")  //promise of response
+        .then(res => res.json()) //promise of json data
+        .then(json => displayLesson(json.data))
+};
+
+const displayLesson = (lessons) => {
+    console.log(lessons)
+}
+loadLesson();
